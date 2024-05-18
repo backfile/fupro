13a451222db33c4ed8bd9d21c6b38a8070a2cca9
Franco Caraffa <francocaraffa2005@gmail.com>
2024-05-18 13:36:44 -0300

    detalles

--- struct_punteros.cpp
+++ struct_punteros.cpp
@@ -8,21 +8,38 @@ struct Persona{ //Struct
 	int edad;
 	float nota;
 	string apellido;
-}persona1 = {"Agustin", 18, 17.5, "Fernandez"};
+}persona1 = {"Agustin", 18, 17.5, "Fernandez"}; //Inicializo
+
+struct Perro{
+	int edad;
+	string owner;
+	
+	Perro()
+	{// Contructor para la estructura "Perro"
+		edad = 11;
+		owner = "Franco";
+	}
+};
 
 int main() {
 	int *dir_age; //Puntero
 	
-	dir_age = &persona1.edad;
+	//Probando dircciones en memoria
+	dir_age = &persona1.edad; 
 	cout << &persona1 << endl;
 	cout << &persona1.nombre << endl;
 	cout << dir_age << endl;
 	cout << &persona1.nota << endl;
 	cout << &persona1.apellido << endl;
 	
+	// Estructura con contructor
+	Perro lolo;
+	cout << lolo.edad <<endl;
+	cout << lolo.owner << endl;
 	
 	
 	
+
 	return 0;
 }
 
